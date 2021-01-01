c3eb8fc4ebfcff0f9eda624d8468f3b3e5511b95
cheng <1195596605@qq.com>
2021-01-01 11:54:03 +0800

    1-1

---
+++ test/test/test.c
@@ -0,0 +1,45 @@
+typedef struct Node Node;
+struct Node* copyRandomList(struct Node* head) {
+	if (head == NULL)
+		return NULL;
+	Node *cur = head;
+	//在原链表后拷贝一份
+	while (cur)
+	{
+		Node *copyNode = (Node *)malloc(sizeof(Node));
+		copyNode->val = cur->val;
+		copyNode->next = NULL;
+		copyNode->random = NULL;
+
+		Node *next = cur->next;
+		copyNode->next = next;
+		cur->next = copyNode;
+		cur = next;
+	}
+	cur = head;
+	//拷贝random
+	while (cur)
+	{
+		Node *copyNode = cur->next;
+		if (cur->random)
+			copyNode->random = cur->random->next;
+		else
+			copyNode->random = NULL;
+		cur = cur->next->next;
+	}
+	cur = head;
+	Node *newnode = head->next;
+	//分开链接
+	while (cur)
+	{
+		Node *copyNode = cur->next;
+		Node *next = copyNode->next;
+		cur->next = next;
+		if (next)
+			copyNode->next = next->next;
+		else
+			copyNode->next = NULL;
+		cur = next;
+	}
+	return newnode;
+}
